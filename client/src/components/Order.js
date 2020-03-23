d14e841c8767fffc60aea6f3f2c29f1714012b62
JeremyLoh <jeremyloh03@gmail.com>
2020-03-23 21:56:49 +0800

    Add css stylesheet for Order component

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../styles/Order.css";
 
 class Order extends Component {
   state = {};
@@ -13,10 +14,26 @@ class Order extends Component {
     return (
       <React.Fragment>
         <div>
-          <h3>Order Number: </h3> <p>{orderNumber}</p>
-          <h3>Customer Name: </h3> <p>{cname}</p>
-          <h3>Delivery Location: </h3> <p>{deliveryLocation}</p>
-          <h3>Restaurant Location: </h3> <p>{restaurantLocation}</p>
+          <div className="row">
+            <h3>Order Number: </h3>
+            <p>{orderNumber}</p>
+          </div>
+
+          <div className="row">
+            <h3>Customer Name: </h3>
+            <p>{cname}</p>
+          </div>
+
+          <div className="row">
+            <h3>Delivery Location: </h3>
+            <p>{deliveryLocation}</p>
+          </div>
+
+          <div className="row">
+            <h3>Restaurant Location: </h3>
+            <p>{restaurantLocation}</p>
+          </div>
+
           <button>Accept</button>
         </div>
       </React.Fragment>

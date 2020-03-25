003804be0833d65d924012ab920c5abea3a8c850
Jeremy Loh <jeremyloh03@gmail.com>
2020-03-25 16:07:39 +0800

    Add FTSelectSchedule component

--- client/src/components/FTSelectSchedule.js
+++ client/src/components/FTSelectSchedule.js
@@ -1,29 +1,55 @@
 import React, { Component } from "react";
-import { Form, FormGroup, CustomInput } from "reactstrap";
+import { Form, FormGroup, Input, Label } from "reactstrap";
+import "../styles/FTSelectSchedule.css";
 
 class FTSelectSchedule extends Component {
   state = {};
   render() {
     return (
       <div>
-        <Form>
-          <FormGroup>
-            <div>
-              <CustomInput
+        <Form className="ft-schedule-form">
+          <FormGroup check>
+            <Label check>
+              <Input
                 type="radio"
                 name="ft-shift"
-                label="Shift 1: 10am to 2pm and 3pm to 7pm"
                 value="Shift 1: 10am to 2pm and 3pm to 7pm"
-                checked
               />
-              <CustomInput
+              Shift 1: 10am to 2pm and 3pm to 7pm
+            </Label>
+          </FormGroup>
+
+          <FormGroup check>
+            <Label check>
+              <Input
                 type="radio"
                 name="ft-shift"
-                label="Shift 2: 11am to 3pm and 4pm to 8pm"
                 value="Shift 2: 11am to 3pm and 4pm to 8pm"
-                checked
               />
-            </div>
+              Shift 2: 11am to 3pm and 4pm to 8pm
+            </Label>
+          </FormGroup>
+
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="radio"
+                name="ft-shift"
+                value="Shift 3: 12pm to 4pm and 5pm to 9pm"
+              />
+              Shift 3: 12pm to 4pm and 5pm to 9pm
+            </Label>
+          </FormGroup>
+
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="radio"
+                name="ft-shift"
+                value="Shift 4: 1pm to 5pm and 6pm to 10pm"
+              />
+              Shift 4: 1pm to 5pm and 6pm to 10pm
+            </Label>
           </FormGroup>
         </Form>
       </div>

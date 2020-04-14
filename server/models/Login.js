70febed3ce852ada97b5ea7a1676a8582854ff2f
JeremyLoh <jeremyloh03@gmail.com>
2020-04-14 22:19:43 +0800

    Update Login model return value

--- server/models/Login.js
+++ server/models/Login.js
@@ -4,14 +4,13 @@ class Login {
   static checkLogin(account_id, account_pass, callback) {
     db.query(
       "SELECT EXISTS(SELECT 1 FROM Accounts WHERE account_id = $1 AND account_pass = $2)",
-      account_id,
-      account_pass,
+      [account_id, account_pass],
       (err, res) => {
         if (err.error) {
           console.err("Could not check login credentials: ", err);
-          return callback(err);
+          return callback(err, res);
         }
-        return callback(res);
+        return callback(err, res);
       }
     );
   }

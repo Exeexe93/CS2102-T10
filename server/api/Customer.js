8e180acb1da2016bfdcd45c09cd6b14ff3c28b5b
Exeexe93 <e0318484@u.nus.edu>
2020-04-15 15:28:38 +0800

    Get rewardpoint from database based on name

--- server/api/Customer.js
+++ server/api/Customer.js
@@ -5,8 +5,16 @@ var router = express.Router();
 
 router.get("/", function (req, res) {
   Customer.getRestaurantList(function (err, restaurantList) {
-    if (err) return res.json(err);
-    return res.json(restaurantList);
+    if (err.error) return res.status(404).json(err);
+    return res.status(200).json(restaurantList);
+  });
+});
+
+router.post("/Profile", function (req, res) {
+  const name = req.body.name;
+  Customer.getProfile(name, (err, profileInfo) => {
+    if (err.error) return res.status(404).json(err);
+    return res.status(200).json(profileInfo);
   });
 });
 
--- server/index.js
+++ server/index.js
@@ -12,18 +12,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/users", require("./api/users"));
-app.use("/RestaurantStaff", require("./api/restaurantstaff.js"));
-app.use("/Customer", require("./api/customer.js"));
+app.use("/users", require("./api/Users"));
+app.use("/RestaurantStaff", require("./api/Restaurantstaff.js"));
+app.use("/Customer", require("./api/Customer.js"));
 app.use("/Login", require("./api/Login.js"));
 
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", ["GET", "POST", "OPTIONS"]);
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};

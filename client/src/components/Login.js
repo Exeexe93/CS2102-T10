56af69e0667bcdf6aa3546af2996110f73ebfe8d
CranstonYeo <e0310976@u.nus.edu>
2020-04-17 01:39:32 +0800

    simple login function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,52 +1,104 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Redirect } from 'react-router';
 import "../styles/Login.css";
+import axios from "axios";
 
 class Login extends Component {
-  handleLogin = () => {
-    let data = {
-      account_id: this.refs.id.value,
-      account_pass: this.refs.password.value
-    };
-    var request = new Request("http://localhost:3001/Login", {
-      method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
-      body: JSON.stringify(data)
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '', 
+      password: '', 
+      redirect: false,
+      redirectLocation: '/Login',
+      errorMessage: ''
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleChangeUserName = this.handleChangeUserName.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
 
-    fetch(request)
-      .then(response => response.json())
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  handleLogin(event) {
+    event.preventDefault();
+    let data = {
+      account_id: this.state.username,
+      account_pass: this.state.password
+    };
+    axios.post('http://localhost:3001/Login', data)
+    .then(res => {
+      if (res.data[0] != null) {
+        let toRedirect;
+        console.log(res.data[0].account_type);
+        switch (res.data[0].account_type) {
+          case "Customer":
+            toRedirect = "/Customer";
+            break;
+          case "RestaurantStaff":
+            toRedirect = "/RestaurantStaffMainPage";
+            break;
+          case "FTRider":
+            toRedirect = "/FTRiderMainPage";
+            break;
+          case "PTRider":
+            toRedirect = "/PTRiderMainPage";
+            break;
+          case "FDSManager":
+            toRedirect = "/FDSManager";
+            break;
+          default:
+            toRedirect = "/Login";
+            break;
+        }
+        this.setState({redirect: true, redirectLocation: toRedirect});
+      } else {
+        this.setState({errorMessage: "Invalid username or password."})
+      }
+    })
   };
 
+  handleChangeUserName(event) {
+    this.setState({username: event.target.value})
+  }
+
+  handleChangePassword(event) {
+    this.setState({password: event.target.value})
+  }
+
+  handleSignUp() {
+    this.setState({redirect: true, redirectLocation: "/"});
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirectLocation} />;
+    }
     return (
       <div className="login-form">
         <h1>Login</h1>
-        <form action="auth" method="POST">
+        <form onSubmit={this.handleLogin}>
           <input
             type="text"
             name="username"
-            ref="id"
+            value={this.state.username}
+            onChange={this.handleChangeUserName}
             placeholder="Username"
             required
           />
           <input
             type="password"
             name="password"
-            ref="password"
+            value={this.state.password}
+            onChange={this.handleChangePassword}
             placeholder="Password"
             required
           />
-          <input type="submit" value="LOGIN" onClick={this.handleLogin} />
+          { this.state.errorMessage && <h5 className="text-danger"> { this.state.errorMessage } </h5> }
+          <input type="submit" value="Submit" />
           <span>
             Don't have an account?
-            <Link to="/"> Sign up here</Link>
+            Sign up{' '}
+            <button onClick={this.handleSignUp}>here</button>
           </span>
         </form>
       </div>
--- server/models/Login.js
+++ server/models/Login.js
@@ -3,12 +3,11 @@ const db = require("../database/index.js");
 class Login {
   static checkLogin(account_id, account_pass, callback) {
     db.query(
-      "SELECT EXISTS(SELECT 1 FROM Accounts WHERE account_id = $1 AND account_pass = $2)",
+      "SELECT * from accounts where account_id = $1 AND account_pass = $2",
       [account_id, account_pass],
       (err, res) => {
         if (err.error) {
-          console.log("Could not check login credentials: ", err);
-          return callback(err, res);
+          return callback(err);
         }
         return callback(err, res);
       }

b8f3a51cc7aea12d3934b407b46a4473b989e5f4
JeremyLoh <jeremyloh03@gmail.com>
2020-04-30 19:05:58 +0800

    Signup API: Add createAccount backend method

--- server/api/Signup.js
+++ server/api/Signup.js
@@ -22,4 +22,25 @@ router.post("/checkAvailableAccountId", (request, response) => {
   });
 });
 
+router.post("/createAccount", (request, response) => {
+  const account_id = request.body.account_id;
+  const name = request.body.name;
+  const account_password = request.body.account_password;
+  const account_type = request.body.account_type;
+  const selected_restaurant = request.body.selected_restaurant;
+  Signup.createAccount(
+    account_id,
+    name,
+    account_password,
+    account_type,
+    selected_restaurant,
+    (err, result) => {
+      if (err.error) {
+        return response.status(404).json(err);
+      }
+      return response.status(200).json(result);
+    }
+  );
+});
+
 module.exports = router;

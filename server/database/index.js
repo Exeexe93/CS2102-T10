ea7d88ed430701bcd8f4f306f40a0e3353f87968
Exeexe93 <e0318484@u.nus.edu>
2020-04-30 19:47:48 +0800

    Fixed the bug for the program crash

--- server/database/index.js
+++ server/database/index.js
@@ -19,14 +19,15 @@ class Database {
   query(query, ...args) {
     this.pool.connect((err, db, done) => {
       if (err) {
-        throw err;
+        console.error(err);
+        return callback({ error: "Unable to connect to database!" }, null);
       }
       const params = args.length === 2 ? args[0] : [];
       const callback = args.length === 1 ? args[0] : args[1];
       db.query(query, params, (err, res) => {
         done();
         if (err) {
-          console.log(err.stack);
+          console.error(err.stack);
           return callback({ error: "Database error..." }, null);
         }
         callback({}, res.rows);

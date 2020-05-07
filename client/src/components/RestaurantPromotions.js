9cfdaea871acab02a151bf0b6fad05116d2a310c
Exeexe93 <e0318484@u.nus.edu>
2020-05-07 11:08:14 +0800

    Change navlink to link for restaurantstaffs

--- client/src/components/RestaurantPromotions.js
+++ client/src/components/RestaurantPromotions.js
@@ -1,207 +1,266 @@
 import React, { Component } from "react";
-import 'react-dates/initialize';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormGroup from 'react-bootstrap/FormGroup'
-import InputGroup from 'react-bootstrap/InputGroup'
-import axios from 'axios';
+import "react-dates/initialize";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
+import InputGroup from "react-bootstrap/InputGroup";
+import axios from "axios";
 import { MdHome } from "react-icons/md";
-import { Navbar, NavbarBrand, Nav, NavLink } from "reactstrap";
+import { Navbar, NavbarBrand, Nav } from "reactstrap";
 import { RiLogoutBoxLine } from "react-icons/ri";
-import { DateRangePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
 import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class RestaurantPromotions extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            promos: [],
-            rest_id: this.props.location.state.rest_id,
-            startDate: null,
-            endDate: null,
-            promotion_type: "percent"
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      promos: [],
+      rest_id: this.props.location.state.rest_id,
+      startDate: null,
+      endDate: null,
+      promotion_type: "percent",
+    };
+  }
 
-    getPromo() {
-        let data = { rest_id: this.state.rest_id };
-        axios.post('http://localhost:3001/RestaurantStaff/getPromo', data)
-        .then(res => this.setState({ promos: res.data }));
+  getPromo() {
+    let data = { rest_id: this.state.rest_id };
+    axios
+      .post("http://localhost:3001/RestaurantStaff/getPromo", data)
+      .then((res) => this.setState({ promos: res.data }));
+  }
+
+  componentDidMount() {
+    this.getPromo();
+  }
+
+  handleSubmit = (event) => {
+    let form = event.target;
+    event.preventDefault();
+    let new_promo = {
+      creator_id: this.props.location.state.account_id,
+      details: form.elements.promotion_details.value,
+      category: "Restaurant",
+      promo_type: this.state.promotion_type,
+      discount_value: Math.round(form.elements.discount.value),
+      trigger_value:
+        "$" + parseFloat(form.elements.minimum_spending.value).toFixed(2),
+      start_time: this.state.startDate._d
+        .toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        })
+        .replace(/ /g, "-"),
+      end_time: this.state.endDate._d
+        .toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        })
+        .replace(/ /g, "-"),
     };
 
-    componentDidMount() {
-        this.getPromo();
-    }
+    axios
+      .post("http://localhost:3001/RestaurantStaff/addPromo", new_promo)
+      .then(() => console.log("Promo Created"))
+      .catch((err) => {
+        console.error(err);
+      });
+    this.setState({
+      promos: [...this.state.promos, new_promo],
+    });
+    form.reset();
+  };
 
-    handleSubmit = (event) => {
-        let form = event.target;
-        event.preventDefault();
-        let new_promo = {
-            creator_id: this.props.location.state.account_id,
-            details: form.elements.promotion_details.value,
-            category: "Restaurant",
-            promo_type: this.state.promotion_type,
-            discount_value: Math.round(form.elements.discount.value),
-            trigger_value: "$" + parseFloat(form.elements.minimum_spending.value).toFixed(2),
-            start_time: this.state.startDate._d.toLocaleDateString('en-GB', {day: '2-digit', month: 'short', year: 'numeric'}).replace(/ /g, '-'),
-            end_time: this.state.endDate._d.toLocaleDateString('en-GB', {day: '2-digit', month: 'short', year: 'numeric'}).replace(/ /g, '-')
-        }
-
-        axios.post('http://localhost:3001/RestaurantStaff/addPromo', new_promo)
-        .then(() => console.log('Promo Created'))
-        .catch(err => {
-            console.error(err);
-        });
-        this.setState ({ 
-            promos: [...this.state.promos, new_promo]
-        });
-        form.reset();
-    }
-
-    handleHomeNavigation = () => {
-        this.props.history.push({
-            pathname: '/RestaurantStaffMainPage',
-            state: {
-                account_id: this.props.location.state.account_id
-            }
-        });
+  handleHomeNavigation = () => {
+    return {
+      pathname: "/RestaurantStaffMainPage",
+      state: {
+        account_id: this.props.location.state.account_id,
+      },
     };
+  };
 
-    renderItem = (promo, index) => {
-        return(
-            <tr key={promo.promo_id}>
-                <td>{index + 1}</td>
-                <td>{promo.start_time}</td>
-                <td>{promo.end_time}</td>
-                <td>{promo.details}</td>
-                <td>{promo.promo_type === "percent" ? "Percentage" : "Flatrate"}</td>
-                <td>{promo.promo_type === "flat-rate" ? "$" : ""}{promo.discount_value}{promo.promo_type === "percent" ? "%" : ""}</td>
-                <td>{promo.trigger_value}</td>
-            </tr>
-        )
-    }
-    
-    handleSelect = event => {
-        if (event.target.value === "Percentage") {
-            this.setState({promotion_type: "percent"})
-        } else if (event.target.value === "Flatrate") {
-            this.setState({promotion_type: "flat-rate"})
-        }
-    }
+  renderItem = (promo, index) => {
+    return (
+      <tr key={promo.promo_id}>
+        <td>{index + 1}</td>
+        <td>{promo.start_time}</td>
+        <td>{promo.end_time}</td>
+        <td>{promo.details}</td>
+        <td>{promo.promo_type === "percent" ? "Percentage" : "Flatrate"}</td>
+        <td>
+          {promo.promo_type === "flat-rate" ? "$" : ""}
+          {promo.discount_value}
+          {promo.promo_type === "percent" ? "%" : ""}
+        </td>
+        <td>{promo.trigger_value}</td>
+      </tr>
+    );
+  };
 
-    renderPercentForm = () => {
-        return <div>
-            <Form.Label>Discount Percent: </Form.Label>
-            <InputGroup>
-                <Form.Control name = "discount" required={true} type="number" min="1" max="100" step="1" data-number-to-fixed="0" placeholder="Discount Percent"/>
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="inputGroupPrepend">%</InputGroup.Text>
-                </InputGroup.Prepend>
-            </InputGroup>
-        </div>
+  handleSelect = (event) => {
+    if (event.target.value === "Percentage") {
+      this.setState({ promotion_type: "percent" });
+    } else if (event.target.value === "Flatrate") {
+      this.setState({ promotion_type: "flat-rate" });
     }
+  };
 
-    renderFlatrateForm = () => {
-        return <div>
-            <Form.Label>Discount Flatrate: </Form.Label>
-            <InputGroup>
-                <InputGroup.Prepend>
+  renderPercentForm = () => {
+    return (
+      <div>
+        <Form.Label>Discount Percent: </Form.Label>
+        <InputGroup>
+          <Form.Control
+            name="discount"
+            required={true}
+            type="number"
+            min="1"
+            max="100"
+            step="1"
+            data-number-to-fixed="0"
+            placeholder="Discount Percent"
+          />
+          <InputGroup.Prepend>
+            <InputGroup.Text id="inputGroupPrepend">%</InputGroup.Text>
+          </InputGroup.Prepend>
+        </InputGroup>
+      </div>
+    );
+  };
+
+  renderFlatrateForm = () => {
+    return (
+      <div>
+        <Form.Label>Discount Flatrate: </Form.Label>
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            name="discount"
+            required={true}
+            type="number"
+            min="1"
+            step="1"
+            data-number-to-fixed="0"
+            placeholder="Discount Flatrate"
+          />
+        </InputGroup>
+        <Form.Text className="text-muted">
+          Flatrate discount must be a whole number.
+        </Form.Text>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar className="navbar" color="dark" dark>
+          <NavbarBrand> Promotional Campaigns </NavbarBrand>
+
+          <Nav className="mr-auto">
+            <Link to={this.handleHomeNavigation} className="icon">
+              <MdHome />
+              <span> Home</span>
+            </Link>
+          </Nav>
+          <Nav>
+            <Link to={{ pathname: "/Login" }} className="icon">
+              <RiLogoutBoxLine />
+              <span> Logout</span>
+            </Link>
+          </Nav>
+        </Navbar>
+        <div className="content">
+          <div className="container">
+            <h1>Add a new promotion</h1>
+            <form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Form.Label>Promotion Date: </Form.Label> <br />
+                <DateRangePicker
+                  required={true}
+                  startDate={this.state.startDate}
+                  startDateId="your_unique_start_date_id"
+                  endDate={this.state.endDate}
+                  endDateId="your_unique_end_date_id"
+                  onDatesChange={({ startDate, endDate }) =>
+                    this.setState({ startDate, endDate })
+                  }
+                  focusedInput={this.state.focusedInput}
+                  onFocusChange={(focusedInput) =>
+                    this.setState({ focusedInput })
+                  }
+                  displayFormat="DD/MM/YYYY"
+                />
+                <h1 />
+                <Form.Label>Promotion Details: </Form.Label>
+                <Form.Control
+                  name="promotion_details"
+                  required={true}
+                  type="text"
+                  placeholder="Promotion Details"
+                  as="textarea"
+                  rows="3"
+                />
+                <h1 />
+                <Form.Label>Promotion Type: </Form.Label>
+                <Form.Control as="select" custom onChange={this.handleSelect}>
+                  <option>Percentage</option>
+                  <option>Flatrate</option>
+                </Form.Control>
+                <h1 />
+                {this.state.promotion_type === "percent"
+                  ? this.renderPercentForm()
+                  : this.renderFlatrateForm()}
+                <h1 />
+                <Form.Label> Minimum Spending: </Form.Label>
+                <InputGroup>
+                  <InputGroup.Prepend>
                     <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control name = "discount" required={true} type="number" min="1" step="1" data-number-to-fixed="0" placeholder="Discount Flatrate"/>
-            </InputGroup>
-            <Form.Text className="text-muted">
-                    Flatrate discount must be a whole number.
-            </Form.Text>
-        </div>
-    }
-
-    render() {
-        return(
-            <div>
-                <Navbar className="navbar" color="dark" dark>
-                    <NavbarBrand> Promotional Campaigns </NavbarBrand>
-
-                    <Nav className="mr-auto">
-                        <NavLink href="" onClick={this.handleHomeNavigation} className="icon">
-                            <MdHome />
-                            <span> Home</span>
-                        </NavLink>
-                    </Nav>
-                    <Nav>
-                        <NavLink href="/Login" className="icon">
-                            <RiLogoutBoxLine />
-                            <span> Logout</span>
-                        </NavLink>
-                    </Nav>
-                </Navbar>
-                <div className="content">
-                    <div className="container">
-                        <h1>Add a new promotion</h1>
-                        <form onSubmit = {this.handleSubmit}>
-                            <FormGroup>
-                                <Form.Label>Promotion Date: </Form.Label> <br/>
-                                <DateRangePicker
-                                    required={true}
-                                    startDate={this.state.startDate}
-                                    startDateId="your_unique_start_date_id"
-                                    endDate={this.state.endDate}
-                                    endDateId="your_unique_end_date_id"
-                                    onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
-                                    focusedInput={this.state.focusedInput}
-                                    onFocusChange={focusedInput => this.setState({ focusedInput })}
-                                    displayFormat="DD/MM/YYYY"
-                                />
-                                <h1/>
-                                <Form.Label>Promotion Details: </Form.Label>
-                                <Form.Control name = "promotion_details" required={true} type="text" placeholder="Promotion Details" as="textarea" rows="3"/>
-                                <h1/>
-                                <Form.Label>Promotion Type: </Form.Label>
-                                    <Form.Control as="select" custom onChange={this.handleSelect}>
-                                        <option>Percentage</option>
-                                        <option>Flatrate</option>
-                                    </Form.Control>
-                                <h1/>
-                                {this.state.promotion_type === "percent" ? this.renderPercentForm() : this.renderFlatrateForm()}
-                                <h1/>
-                                <Form.Label> Minimum Spending: </Form.Label>
-                                <InputGroup>
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <Form.Control name = "minimum_spending" required={true} type="number" type="number" min="0.01" step="0.01" 
-                                    data-number-to-fixed="2" placeholder="Minimum Spending"/>
-                                </InputGroup>
-                                <br/>
-                                <div className="text-center">
-                                    <Button type="submit"> Add Promo </Button>
-                                </div>
-                            </FormGroup>
-                        </form>
-                        <h1>Ongoing/Past Promotions</h1> <br/>
-                        <Table striped bordered hover>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Start Date</th>
-                                    <th>End Date</th>
-                                    <th>Promotion Details</th>
-                                    <th>Promo Type</th>
-                                    <th>Discount</th>
-                                    <th>Minimum Spending</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.promos.map(this.renderItem)}
-                            </tbody>
-                        </Table>
-                    </div>
+                  </InputGroup.Prepend>
+                  <Form.Control
+                    name="minimum_spending"
+                    required={true}
+                    type="number"
+                    type="number"
+                    min="0.01"
+                    step="0.01"
+                    data-number-to-fixed="2"
+                    placeholder="Minimum Spending"
+                  />
+                </InputGroup>
+                <br />
+                <div className="text-center">
+                  <Button type="submit"> Add Promo </Button>
                 </div>
-            </div>
-        );
-    }
-
+              </FormGroup>
+            </form>
+            <h1>Ongoing/Past Promotions</h1> <br />
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                  <th>Promotion Details</th>
+                  <th>Promo Type</th>
+                  <th>Discount</th>
+                  <th>Minimum Spending</th>
+                </tr>
+              </thead>
+              <tbody>{this.state.promos.map(this.renderItem)}</tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RestaurantPromotions;
--- client/src/components/RestaurantSummaryPage.js
+++ client/src/components/RestaurantSummaryPage.js
@@ -1,122 +1,173 @@
 import React, { Component } from "react";
 import "../styles/RestaurantSummaryPage.css";
-import axios from 'axios';
+import axios from "axios";
 import { MdHome } from "react-icons/md";
-import { Navbar, NavbarBrand, Nav, NavLink, Jumbotron } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, Jumbotron } from "reactstrap";
 import { RiLogoutBoxLine } from "react-icons/ri";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 class RestaurantSummaryPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numOfOrders: "",
-            topItems: [],
-            promos: [],
-            selectedPromo: ""
-        }
-    }
-
-    getNumOfOrders() {
-        let data = { rest_id: this.props.location.state.rest_id };
-        axios.post('http://localhost:3001/RestaurantStaff/getNumOfOrders', data)
-        .then(res => this.setState({ numOfOrders: res.data[0].count }));
-      };
-
-    getTopItems() {
-        let data = { rest_id: this.props.location.state.rest_id };
-        axios.post('http://localhost:3001/RestaurantStaff/getTopItems', data)
-        .then(res => this.setState({ topItems: res.data }));
-    }
-
-    getPromoStats() {
-        let data = { rest_id: this.props.location.state.rest_id };
-        axios.post('http://localhost:3001/RestaurantStaff/getPromoStats', data)
-        .then(res => this.setState({ promos: res.data, selectedPromo: res.data[0] }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      numOfOrders: "",
+      topItems: [],
+      promos: [],
+      selectedPromo: "",
     };
+  }
 
-    componentDidMount() {
-        this.getNumOfOrders();
-        this.getTopItems();
-        this.getPromoStats();
-    }
+  getNumOfOrders() {
+    let data = { rest_id: this.props.location.state.rest_id };
+    axios
+      .post("http://localhost:3001/RestaurantStaff/getNumOfOrders", data)
+      .then((res) => this.setState({ numOfOrders: res.data[0].count }));
+  }
 
-    renderItem = (listItem, index) => {
-        return(
-            <li key={index} className="list-group-item list-group-item-secondary">
-                {index + 1}. {listItem.name} - {listItem.totalquantity} sold
-            </li>
-        )
-    }
+  getTopItems() {
+    let data = { rest_id: this.props.location.state.rest_id };
+    axios
+      .post("http://localhost:3001/RestaurantStaff/getTopItems", data)
+      .then((res) => this.setState({ topItems: res.data }));
+  }
 
-    handleHomeNavigation = () => {
-        this.props.history.push({
-            pathname: '/RestaurantStaffMainPage',
-            state: {
-                account_id: this.props.location.state.account_id
-            }
-        });
+  getPromoStats() {
+    let data = { rest_id: this.props.location.state.rest_id };
+    axios
+      .post("http://localhost:3001/RestaurantStaff/getPromoStats", data)
+      .then((res) =>
+        this.setState({ promos: res.data, selectedPromo: res.data[0] })
+      );
+  }
+
+  componentDidMount() {
+    this.getNumOfOrders();
+    this.getTopItems();
+    this.getPromoStats();
+  }
+
+  renderItem = (listItem, index) => {
+    return (
+      <li key={index} className="list-group-item list-group-item-secondary">
+        {index + 1}. {listItem.name} - {listItem.totalquantity} sold
+      </li>
+    );
+  };
+
+  handleHomeNavigation = () => {
+    return {
+      pathname: "/RestaurantStaffMainPage",
+      state: {
+        account_id: this.props.location.state.account_id,
+      },
     };
+  };
 
-    handleSelect = (event) => {
-        this.setState({selectedPromo: this.state.promos[event.target.value.slice(10, 11) - 1]});
-    }
+  handleSelect = (event) => {
+    this.setState({
+      selectedPromo: this.state.promos[event.target.value.slice(10, 11) - 1],
+    });
+  };
 
-    render() {
-        return(
-            <div>
-                <Navbar className="navbar" color="dark" dark>
-                    <NavbarBrand> Summary Information </NavbarBrand>
-                    <Nav className="mr-auto">
-                        <NavLink href="" onClick={this.handleHomeNavigation} className="icon">
-                            <MdHome />
-                            <span> Home</span>
-                        </NavLink>
-                    </Nav>
-                    <Nav>
-                        <NavLink href="/Login" className="icon">
-                            <RiLogoutBoxLine />
-                            <span> Logout</span>
-                        </NavLink>
-                    </Nav>
-                </Navbar>
-                <Jumbotron>
-                        <div className="centered-container">
-                            <h1 className="display-5">
-                                <span> <b>Total no. of completed orders: {' '}</b>
-                                {this.state.numOfOrders}  </span>
-                            </h1>
-                        </div>
-                </Jumbotron>
+  render() {
+    return (
+      <div>
+        <Navbar className="navbar" color="dark" dark>
+          <NavbarBrand> Summary Information </NavbarBrand>
+          <Nav className="mr-auto">
+            <Link to={this.handleHomeNavigation} className="icon">
+              <MdHome />
+              <span> Home</span>
+            </Link>
+          </Nav>
+          <Nav>
+            <Link to={{ pathname: "/Login" }} className="icon">
+              <RiLogoutBoxLine />
+              <span> Logout</span>
+            </Link>
+          </Nav>
+        </Navbar>
+        <Jumbotron>
+          <div className="centered-container">
+            <h1 className="display-5">
+              <span>
+                {" "}
+                <b>Total no. of completed orders: </b>
+                {this.state.numOfOrders}{" "}
+              </span>
+            </h1>
+          </div>
+        </Jumbotron>
 
-                <div className="body-container">
-                    <h1><u><center>Top food items (up to 5):</center></u></h1><h1/>
-                    <h4>
-                        <ul className="list-group">
-                            {this.state.topItems.map(this.renderItem)}
-                        </ul>
-                    </h4>
-                   
-                    <h2><h1><u><center>Promotion Campaign's Statistics</center></u></h1><h1/>
-                        <h3><Form.Label>Select Promotion: </Form.Label>
-                        <Form.Control as="select" custom onChange={this.handleSelect}>
-                            {this.state.promos.map((promo, index) => <option>Promotion {index + 1}</option>)}
-                        </Form.Control><h1/></h3>
-                        <h4><strong>Promotion Info:</strong><br/>
-                            Period: {this.state.selectedPromo.start_time} {' TO '} {this.state.selectedPromo.end_time}<br/>
-                            Duration: {this.state.selectedPromo.duration} {this.state.selectedPromo.duration > 1 ? "days" : "day"}<br/>
-                            Details: {this.state.selectedPromo.details}<br/>
-                            Discount: {this.state.selectedPromo.promo_type === "flat-rate" ? "$" : ""}{this.state.selectedPromo.discount_value}{this.state.selectedPromo.promo_type === "percent" ? "%" : ""}<br/>
-                            Minimum Spending: {this.state.selectedPromo.trigger_value}<h1/>
-                            <strong>Average number of orders received:</strong><br/>
-                            Per day: {parseFloat(this.state.selectedPromo.num_of_times_used/this.state.selectedPromo.duration).toFixed(3)} orders <br/>
-                            Per hour: {parseFloat(this.state.selectedPromo.num_of_times_used/this.state.selectedPromo.duration/24).toFixed(3)} orders
-                        </h4>
-                    </h2>
-                </div>
-            </div>
-        );
-    }
+        <div className="body-container">
+          <h1>
+            <u>
+              <center>Top food items (up to 5):</center>
+            </u>
+          </h1>
+          <h1 />
+          <h4>
+            <ul className="list-group">
+              {this.state.topItems.map(this.renderItem)}
+            </ul>
+          </h4>
+
+          <h2>
+            <h1>
+              <u>
+                <center>Promotion Campaign's Statistics</center>
+              </u>
+            </h1>
+            <h1 />
+            <h3>
+              <Form.Label>Select Promotion: </Form.Label>
+              <Form.Control as="select" custom onChange={this.handleSelect}>
+                {this.state.promos.map((promo, index) => (
+                  <option>Promotion {index + 1}</option>
+                ))}
+              </Form.Control>
+              <h1 />
+            </h3>
+            <h4>
+              <strong>Promotion Info:</strong>
+              <br />
+              Period: {this.state.selectedPromo.start_time} {" TO "}{" "}
+              {this.state.selectedPromo.end_time}
+              <br />
+              Duration: {this.state.selectedPromo.duration}{" "}
+              {this.state.selectedPromo.duration > 1 ? "days" : "day"}
+              <br />
+              Details: {this.state.selectedPromo.details}
+              <br />
+              Discount:{" "}
+              {this.state.selectedPromo.promo_type === "flat-rate" ? "$" : ""}
+              {this.state.selectedPromo.discount_value}
+              {this.state.selectedPromo.promo_type === "percent" ? "%" : ""}
+              <br />
+              Minimum Spending: {this.state.selectedPromo.trigger_value}
+              <h1 />
+              <strong>Average number of orders received:</strong>
+              <br />
+              Per day:{" "}
+              {parseFloat(
+                this.state.selectedPromo.num_of_times_used /
+                  this.state.selectedPromo.duration
+              ).toFixed(3)}{" "}
+              orders <br />
+              Per hour:{" "}
+              {parseFloat(
+                this.state.selectedPromo.num_of_times_used /
+                  this.state.selectedPromo.duration /
+                  24
+              ).toFixed(3)}{" "}
+              orders
+            </h4>
+          </h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RestaurantSummaryPage;
